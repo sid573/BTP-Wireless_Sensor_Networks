3024297d5b8df82a1dc483aa034ede331c7f7718
Siddhant Sinha <35433185+sid573@users.noreply.github.com>
2020-11-04 20:53:53 +0530

    Env Updated

--- pkg/environment.go
+++ pkg/environment.go
@@ -7,28 +7,37 @@ import (
 
 // Bot for creating each bot
 type Bot struct {
-	ID        int
-	Detection bool
+	ID          int
+	Detection   bool
+	RegionID    int
+	SubRegionID int
 }
 
 // Region for creating different region
 type Region struct {
-	ID          int
-	SubRegionID int
-	Available   float64
-	BotCount    int
+	ID int
+	// More information regarding particular region can be stored
+}
+
+// SubRegion ...
+type SubRegion struct {
+	ID        int
+	RegionID  int
+	Available float64
 }
 
 // SDSController to be used as reciever for functions implemented
 type SDSController struct {
-	Bots    []*Bot
-	Regions []*Region
+	Bots       []*Bot
+	Regions    []*Region
+	SubRegions []*SubRegion
 }
 
 // SDSEnv to create environment for simulation
 func SDSEnv(botCount int, regionCount int, subRegionCount int) *SDSController {
 	bots := make([]*Bot, 0)
 	regions := make([]*Region, 0)
+	subRegions := make([]*SubRegion, 0)
 	// Creating bots for environment
 	for i := 0; i < botCount; i++ {
 		bot := &Bot{
@@ -38,19 +47,24 @@ func SDSEnv(botCount int, regionCount int, subRegionCount int) *SDSController {
 	}
 	// Creating Region and SubRegions
 	for i := 0; i < regionCount; i++ {
+		region := &Region{
+			ID: i,
+		}
 		for j := 0; j < subRegionCount; j++ {
 			available := rand.Float64()
-			region := &Region{
-				ID:          i,
-				SubRegionID: j,
-				Available:   available,
+			subRegion := &SubRegion{
+				ID:        j,
+				RegionID:  i,
+				Available: available,
 			}
-			regions = append(regions, region)
+			subRegions = append(subRegions, subRegion)
 		}
+		regions = append(regions, region)
 	}
 	controller := &SDSController{
-		Bots:    bots,
-		Regions: regions,
+		Bots:       bots,
+		Regions:    regions,
+		SubRegions: subRegions,
 	}
 	return controller
 }
@@ -65,5 +79,9 @@ func (env *SDSController) DisplayEnv() {
 	fmt.Println("Details of all regions in the environment")
 	for _, region := range env.Regions {
 		fmt.Println(*region)
+	}
+	fmt.Println("Details of all sub-regions in the environment")
+	for _, subRegion := range env.SubRegions {
+		fmt.Println(*subRegion)
 	}
 }
